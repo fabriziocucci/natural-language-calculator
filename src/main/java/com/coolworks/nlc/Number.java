9b7709cb3265450f76e776f24b42f9b5bfcfe777
Fabrizio Cucci <fabrizio.cucci@yahoo.com>
2015-02-05 01:56:33 +0100

    Introduced javadocs for Number and Operator

--- src/main/java/com/coolworks/nlc/Number.java
+++ src/main/java/com/coolworks/nlc/Number.java
@@ -1,17 +1,31 @@
 package com.coolworks.nlc;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 enum Number {
 	
 	ZERO("zero", 0), ONE("one", 1), TWO("two", 2), THREE("three", 3), FOUR("four", 4), FIVE("five", 5), SIX("six", 6), SEVEN("seven", 7), EIGHT("eight", 8), NINE("nine", 9);
 	
-	private static final Map<String, Number> nameToNumber = Arrays.stream(Number.values()).collect(Collectors.toMap(number -> number.name, Function.<Number> identity()));
+	/**
+	 * Dictionary used to retrieve a specific {@link Number} enum constant given its human readable string.
+	 */
+	private static final Map<String, Number> nameToNumber = new HashMap<>();
+	static {
+		Arrays.stream(Number.values()).forEach(number -> {
+			nameToNumber.put(number.name, number);
+		});
+	}
 	
+	/**
+	 * Number name as human readable string.
+	 */
 	private final String name;
+	
+	/**
+	 * Number value as integer.
+	 */
 	private final int value;
 
 	private Number(String name, int value) {
@@ -19,10 +33,17 @@ enum Number {
 		this.value = value;
 	}		
 	
-	public String getSymbol() {
+	@Override
+	public String toString() {
 		return String.valueOf(value);
 	}
 
+	/**
+	 * Static facility used to retrieve a specific {@link Number} enum constant given its human readable string.
+	 * 
+	 * @param numberName is the human readable string of the number
+	 * @return the enum constant identified by its human readable string
+	 */
 	public static Number fromName(String numberName) {
 		Number number = nameToNumber.get(numberName);
 		if (number != null) {
@@ -32,8 +53,14 @@ enum Number {
 		}
 	}
 	
-	public static boolean isNumber(String numberName) {
-		return nameToNumber.containsKey(numberName);
+	/**
+	 * Static facility used to verify that a particular token is actually a number.
+	 * 
+	 * @param token is the token to check for validity
+	 * @return whether the input token is a number
+	 */
+	public static boolean isNumber(String token) {
+		return nameToNumber.containsKey(token);
 	}
 	
 }
--- src/main/java/com/coolworks/nlc/Operator.java
+++ src/main/java/com/coolworks/nlc/Operator.java
@@ -37,6 +37,9 @@ enum Operator {
 		}
 	};
 	
+	/**
+	 * Dictionary used to retrieve a specific {@link Operator} enum constant given any of its natural language alias.
+	 */
 	private static final Map<String, Operator> aliasToOperator = new HashMap<>(); 
 	static {
 		Arrays.stream(Operator.values()).forEach(operator -> {
@@ -46,8 +49,19 @@ enum Operator {
 		});
 	}
 	
+	/**
+	 * Symbol representing the arithmetic operator (e.g. '+', '-', '*', '/').
+	 */
 	private final String symbol;
+	
+	/**
+	 * Integer value representing the arithmetic operator precedence.
+	 */
 	private final int precedence;
+	
+	/**
+	 * Set of permitted natural language aliases for the arithmetic operator.
+	 */
 	private final Set<String> aliases;
 	
 	private Operator(String symbol, int precedence, String... aliases) {
@@ -58,12 +72,36 @@ enum Operator {
 		Collections.addAll(this.aliases, aliases);
 	}
 	
-	public String getSymbol() {
+	@Override
+	public String toString() {
 		return symbol;
 	}
 
+	/**
+	 * Simply compares this arithmetic operator precedence with another arithmetic operator.
+	 * 
+	 * @param that is the other arithmetic operator involved in the comparison
+	 * @return whether this arithmetic operator has less priority than the other one
+	 */
+	public boolean hasLessPriorityThan(Operator that) {
+		return this.precedence < that.precedence;
+	}
+	
+	/**
+	 * Each arithmetic operator implements this method accordingly.
+	 * 
+	 * @param operand1 is the first operand involved in the arithmetic operation
+	 * @param operand2 is the second operand involved in the arithmetic operation
+	 * @return the result of applying the arithmetic operator to the provided operands
+	 */
 	public abstract double apply(double operand1, double operand2);
 	
+	/**
+	 * Static facility used to retrieve a specific {@link Operator} enum constant given any of its natural language alias.
+	 * 
+	 * @param operatorAlias is the natural language alias of the arithmetic operator
+	 * @return the enum constant identified by its natural language alias
+	 */
 	public static Operator fromAlias(String operatorAlias) {
 		Operator operator = aliasToOperator.get(operatorAlias);
 		if (operator != null) {
@@ -73,12 +111,14 @@ enum Operator {
 		}
 	}
 	
-	public static boolean isOperator(String operatorAlias) {
-		return aliasToOperator.containsKey(operatorAlias);
+	/**
+	 * Static facility used to verify that a particular token is actually an arithmetic operator.
+	 * 
+	 * @param token is the token to check for validity
+	 * @return whether the input token is an arithmetic operator
+	 */
+	public static boolean isOperator(String token) {
+		return aliasToOperator.containsKey(token);
 	}
 
-	public boolean hasLessPriorityThan(Operator that) {
-		return this.precedence < that.precedence;
-	}
-	
 }
